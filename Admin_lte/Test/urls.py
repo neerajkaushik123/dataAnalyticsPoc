ab180a1e3f80af20bab99b0821a87b9d444f6a39
Samaksh <72433898+SaMaKsH-exe@users.noreply.github.com>
2023-04-06 19:30:51 +0200

    Trying to make a log in

--- Admin_lte/Test/urls.py
+++ Admin_lte/Test/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns =   [
-    path('', views.render_page)
+    path('', views.render_page),
+    path('login/', views.authenciate_view , name = 'login')
+    
 ]  
--- Admin_lte/Test/views.py
+++ Admin_lte/Test/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse, request
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 
 def render_page(request):
     return render(request, 'Login Page Github.html')
-#hahahah
+
+
+def authenciate_view(request):
+    username=request.POST.get('Samaksh')
+    password= request.POST.get('pls work')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('authenticated')
+    else:
+        return render(request, 'Login Page Github.html', {'error':'invalid login'})
