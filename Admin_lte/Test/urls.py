7e17c2e8c1f654080e5d5ab8b60ffd0f61ca3e13
Samaksh <72433898+SaMaKsH-exe@users.noreply.github.com>
2023-04-11 18:32:13 +0200

    Password storing to database

--- Admin_lte/Test/urls.py
+++ Admin_lte/Test/urls.py
@@ -1,20 +1,13 @@
 
 from . import views
 from django.urls import path
-from .views import yt_vid, login_user
 
-<<<<<<< Updated upstream
 
 
 urlpatterns =   [
     path('', views.login_user, name = 'login'),
     path('yt/', views.yt_vid, name = 'yt_vid'),
-    path('pswd/', views.password_page, name = 'password'),
+    path('pswd/', views.forgot_password, name = 'password'),
   
 ]  
-=======
-urlpatterns = [
-    path('yt/', yt_vid, name='yt_vid'),
-    path('', login_user, name='login'),
-]
->>>>>>> Stashed changes
+
